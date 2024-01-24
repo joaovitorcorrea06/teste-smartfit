6be24c465ba79b9f81c8114a87b6bcca557a9c7f
João Vitor Corrêa <joaov1997@hotmail.com>
2024-01-24 08:09:27 -0300

    Update Label.jsx

--- src/components/Label.jsx
+++ src/components/Label.jsx
@@ -15,10 +15,10 @@ import ForbiddenLockerRoom from '../assets/images/forbidden-lockerroom.png'
 
 const Label = () => {
   return (
-    <div className='w-[90%]  md:w-[65%] mx-auto bg-[#f5f5f5] rounded mb-20 flex flex-col md:flex-row lg:flex-row justify-between p-6'>
-      <div className='w-[100%] md:w-[22%] lg:w-[22%] text-center mb-4 md:mb-0 lg:mb-0'>
+    <div className='w-[90%]  mx-auto bg-[#f5f5f5] rounded mb-20 flex flex-col md:flex-row lg:flex-row justify-between p-6 flex-wrap'>
+      <div className=' text-center mb-4 md:mb-0 lg:mb-0'>
         <p className='font-bold text-xl mb-4' >Máscara</p>
-        <div className='flex items-center justify-center px-10 gap-4'>
+        <div className='flex items-center justify-center px-10 gap-4 flex-wrap'>
           <div className='flex flex-col items-center'>
             <img src={RequiredMask} alt='Required Mask' className='w-14 md:w-24' />
             <p className='text-xl'>Obrigatório</p>
@@ -29,9 +29,9 @@ const Label = () => {
           </div>
         </div>
       </div>
-      <div className='w-[100%] md:w-[22%] lg:w-[22%] text-center mb-4 md:mb-0 lg:mb-0'>
+      <div className=' text-center mb-4 md:mb-0 lg:mb-0'>
         <p className='font-bold text-xl mb-4' >Toalha</p>
-        <div className='flex items-center justify-center px-10 gap-4'>
+        <div className='flex items-center justify-center px-10 gap-4 flex-wrap'>
           <div className='flex flex-col items-center'>
             <img src={RequiredTowel} alt='Required Mask' className='w-14 md:w-24' />
             <p className='text-xl'>Obrigatório</p>
@@ -42,9 +42,9 @@ const Label = () => {
           </div>
         </div>
       </div>
-      <div className='w-[100%] md:w-[22%] lg:w-[22%] text-center mb-4 md:mb-0 lg:mb-0'>
+      <div className=' text-center mb-4 md:mb-0 lg:mb-0'>
         <p className='font-bold text-xl mb-4' >Bebedouro</p>
-        <div className='flex items-center justify-center px-10 gap-4'>
+        <div className='flex items-center justify-center px-10 gap-4 flex-wrap'>
           <div className='flex flex-col items-center justify-center'>
             <img src={ParcialFountain} alt='Required Mask' className='w-14 md:w-24' />
             <p className='text-xl'>Parcial</p>
@@ -55,9 +55,9 @@ const Label = () => {
           </div>
         </div>
       </div>
-      <div className='w-[100%] md:w-[30%] lg:w-[30%] text-center'>
+      <div className=' text-center'>
         <p className='font-bold text-xl mb-4' >Vestiário</p>
-        <div className='flex items-center justify-between px-10 gap-4'>
+        <div className='flex items-center justify-center px-10 gap-4 flex-wrap'>
           <div className='flex flex-col items-center'>
             <img src={RequiredLockerRoom} alt='Required Mask' className='w-14 md:w-24' />
             <p className='text-xl'>Liberado</p>
